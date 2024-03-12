6c509d26353ca0016495f1b074bf23e39ed7c611
Flame <matteo.mocera@gmail.com>
2024-03-12 15:31:23 +0000

    Initial Structure

---
+++ plugin.php
@@ -0,0 +1,9 @@
+<?php 
+/**
+ * Plugin Name: PMWP - Pimp My WordPress
+ * Description: Add the power to enable or disable some code snippets without the needs to write a single line of code
+ * Version: 1.0.0
+ * Author: Flame
+ * Author URI: http://pixelfoundry.studio
+ * Text Domain: pmwp
+   */
